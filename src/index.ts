0900a3ee0597955dab1b6e54c7545374aa78a6a1
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-03-13 09:26:13 +0000

    stash

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 /**
  * The entrypoint for the action.
  */
-// import { run } from './main'
 import { run } from './nexus-sync';
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
